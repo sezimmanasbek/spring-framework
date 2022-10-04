c53037cb19e55262b7805051091b7f4a79c1e87a
Sezi <sezimka1205@gmail.com>
2022-10-04 15:20:20 -0400

    Spring Controller and Model

--- Spring-08-Spring-MVC-Model/src/main/java/com/example/spring08springmvcmodel/Controller/StudentController.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/example/spring08springmvcmodel/Controller/StudentController.java
@@ -1,8 +1,13 @@
 package com.example.spring08springmvcmodel.Controller;
 
+import com.example.spring08springmvcmodel.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @Controller
 public class StudentController {
@@ -12,9 +17,20 @@ public class StudentController {
 
         model.addAttribute("name","Cydeo");
         model.addAttribute("course","MVC");
+        String subject = "Collections";
+        model.addAttribute("subject",subject);
+         int studentID = new Random().nextInt();
+        model.addAttribute("id",studentID);
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(8);
+        numbers.add(5);
+        numbers.add(12);
+        numbers.add(25);
+        numbers.add(100);
+        model.addAttribute("list",numbers);
 
-
-
+        Student student = new Student(1,"Mike","Smith");
+        model.addAttribute("student",student);
         return "student/welcome";
     }
 }
--- Spring-08-Spring-MVC-Model/src/main/java/com/example/spring08springmvcmodel/Spring08SpringMvcModelApplication.java
+++ Spring-08-Spring-MVC-Model/src/main/java/com/example/spring08springmvcmodel/Spring08SpringMvcModelApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Spring08SpringMvcModelApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         SpringApplication.run(Spring08SpringMvcModelApplication.class, args);
     }
 
