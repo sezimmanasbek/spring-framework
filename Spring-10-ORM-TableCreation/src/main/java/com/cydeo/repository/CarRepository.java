cfb6b2740410520ba6cb30206095c705efe23169
Sezi <sezimka1205@gmail.com>
2022-10-23 21:24:11 -0400

    Joining Tables, Object Relationship OneToOne, OneToMany, ManyToOne, ManyToMany

--- Spring-10-ORM-TableCreation/src/main/java/com/cydeo/repository/CarRepository.java
+++ Spring-10-ORM-TableCreation/src/main/java/com/cydeo/repository/CarRepository.java
@@ -4,7 +4,7 @@ import com.cydeo.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//@Repository
+@Repository
 public interface CarRepository extends JpaRepository<Car,Long> {
 
 }
